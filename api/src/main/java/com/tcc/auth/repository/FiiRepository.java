d68205472c31327dde0bb2820f56678ba8a2cccc
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-10-22 16:30:22 -0300

    Add @Repository annotation to FiiRepository

--- api/src/main/java/com/tcc/auth/repository/FiiRepository.java
+++ api/src/main/java/com/tcc/auth/repository/FiiRepository.java
@@ -1,8 +1,10 @@
 package com.tcc.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tcc.auth.model.fii.Fii;
 
+@Repository
 public interface FiiRepository extends JpaRepository<Fii, Integer> {
 }
