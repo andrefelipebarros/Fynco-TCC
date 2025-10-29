bd219d195b4faef2a6627b6d8e8ddd9dd465d39b
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-10-28 22:54:12 -0300

    fix: Change redirect method to return ResponseEntity

--- api/src/main/java/com/tcc/auth/controller/HomeController.java
+++ api/src/main/java/com/tcc/auth/controller/HomeController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping({"/", ""})
-    public String toString() {
-        return "redirect:http://localhost:3000/questionnaire";
+    public ResponseEntity<Void> redirectToFrontend() {
+        // Cria o cabeçalho "Location" com a URL de destino
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("http://localhost:3000/questionnaire"));
+        
+        // Retorna uma resposta com status 302 (Found) e o cabeçalho
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
-    
 }
