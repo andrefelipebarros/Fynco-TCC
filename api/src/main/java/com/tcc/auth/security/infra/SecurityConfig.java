0819319532ee9af4ed6d5c8b9eae2e58d05d67d9
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-11-03 21:49:28 -0300

    Update SecurityConfig.java

--- api/src/main/java/com/tcc/auth/security/infra/SecurityConfig.java
+++ api/src/main/java/com/tcc/auth/security/infra/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://fynco.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
